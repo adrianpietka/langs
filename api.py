7b68c9760bbb64f59d7ba2324b8e90ee713ff2aa
Adrian Pietka <adrian@pietka.me>
2015-11-03 15:48:21 +0100

    GitHub global stats

--- api.py
+++ api.py
@@ -5,9 +5,52 @@ from config import config
 
 app = Flask(__name__)
 
+def get_processed_repositories_number():
+    with g.db.cursor() as cursor:
+        sql = ('SELECT COUNT(*) AS total '
+            'FROM github_index '
+            'WHERE metadata_updated_at IS NOT NULL')
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        return result['total']
+
+def get_repositories_number():
+    with g.db.cursor() as cursor:
+        sql = ('SELECT COUNT(*) AS total '
+            'FROM github_index')
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        return result['total']
+
+def get_oldest_repository():
+    with g.db.cursor() as cursor:
+        sql = ('SELECT full_name, created_at '
+            'FROM github_index '
+            'WHERE created_at IS NOT NULL '
+            'ORDER BY created_at ASC')
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        return dict(name=result['full_name'], created=result['created_at'].isoformat())
+
+def get_latest_repository():
+    with g.db.cursor() as cursor:
+        sql = ('SELECT full_name, created_at '
+            'FROM github_index '
+            'WHERE created_at IS NOT NULL '
+            'ORDER BY created_at DESC')
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        return dict(name=result['full_name'], created=result['created_at'].isoformat())
+
 @app.route('/', methods=['GET'])
 def dashboard():
     return render_template('dashboard.html')
+
+@app.route('/github/stats')
+def github_stats():
+    stats = dict(oldest=get_oldest_repository(), latest=get_latest_repository(),
+        total=get_repositories_number(), processed=get_processed_repositories_number())
+    return jsonify(data=stats)
 
 @app.route('/github/languages')
 def github_top_languages():
